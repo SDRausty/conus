d69857921d3b04036e68764805f58a892fad70ae
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-06-14 06:35:49 -0400

    commit 622533949 on 20200614

--- get.bash
+++ get.bash
@@ -6,6 +6,11 @@ set -eu
 _CGEOS_() {
 	curl https://cdn.star.nesdis.noaa.gov/$GOES/ABI/FD/GEOCOLOR/1808x1808.jpg -o $(date +%s).1808x1808.$GOES.jpg 
 }
+
+_PRINTSIG_ () {
+	printf "%s\\n" "SIGNAL $1 GENERATED:  Continuing...  "
+}
+
 DATE="$(date +%Y%m%d)"
 [ ! -d "$DATE" ] && mkdir -p "$DATE" 
 cd "$DATE" 
@@ -13,13 +18,13 @@ wget https://www.wpc.ncep.noaa.gov//noaa/national_forecast.jpg
 wget https://radar.weather.gov/Conus/Loop/NatLoop_Small.gif
 wget https://graphical.weather.gov/images/conus/MaxT{1..7}_conus.png
 wget https://graphical.weather.gov/images/conus/MinT{1..7}_conus.png
-wget https://graphical.weather.gov/images/conus/T{1..27}_conus.png ||:
-wget https://graphical.weather.gov/images/conus/QPF{1..12}_conus.png ||:
-wget https://graphical.weather.gov/images/conus/WindSpd{1..27}_conus.png ||:
-wget https://graphical.weather.gov/images/conus/WaveHeight{1..20}_conus.png ||:
-wget https://graphical.weather.gov/images/conus/ApparentT{1..27}_conus.png ||:
+wget https://graphical.weather.gov/images/conus/T{1..27}_conus.png || _PRINTSIG_ 27
+wget https://graphical.weather.gov/images/conus/QPF{1..12}_conus.png || _PRINTSIG_ 29
+wget https://graphical.weather.gov/images/conus/WindSpd{1..27}_conus.png || _PRINTSIG_ 31
+wget https://graphical.weather.gov/images/conus/WaveHeight{1..20}_conus.png || _PRINTSIG_ 33
+wget https://graphical.weather.gov/images/conus/ApparentT{1..27}_conus.png || _PRINTSIG_ 35
 GOES="GOES16"
-_CGEOS_ ||:
+_CGEOS_ || _PRINTSIG_ 37
 GOES="GOES17"
-_CGEOS_ ||:
+_CGEOS_ || _PRINTSIG_ 39
 # get.bash EOF
